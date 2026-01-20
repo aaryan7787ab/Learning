e8c4ce784e11327c6ed89f7bc2f4358c17fb2ee9
Aryan Bhardwaj <baaryan428@gmail.com>
2026-01-20 17:41:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,21 @@
 let name=" aryan bhardwaj ";
 for ( let i= 1; i<=5;i++){
     console.log([i],name);
+
+};
+
+// for each
+
+let name = "aryan";
+let fullname = Array.from(name);
+
+fullname.forEach((Element) => {
+  console.log(Element);
+});
+
+//for in loop
+let num = [1, 2, 34, 56, 78, 9];
+
+for (let i in num) {
+  console.log(num[i]);
 }
